3d95bb6e840d70d308c3ec0d80c46a9aa63348e3
uhsr <uhsr@eteb.me>
2025-07-11 20:32:18 +0700

    Add nftmetadataindexerproject.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ internal/nftmetadataindexerproject/nftmetadataindexerproject.go
@@ -0,0 +1,36 @@
+// internal/nftmetadataindexerproject/nftmetadataindexerproject.go
+package nftmetadataindexerproject
+
+import (
+"log"
+"os"
+)
+
+type App struct {
+verbose bool
+logger *log.Logger
+}
+
+func NewApp(verbose bool) *App {
+app := &App{
+verbose: verbose,
+logger: log.New(os.Stdout, "", log.LstdFlags),
+}
+
+if verbose {
+app.logger.SetPrefix("[DEBUG] ")
+} else {
+app.logger.SetPrefix("[INFO] ")
+}
+
+return app
+}
+
+func (a *App) Run() error {
+a.logger.Printf("Starting %s processing", "NftMetadataIndexerProject")
+
+// Add your implementation here
+
+a.logger.Println("Processing completed successfully")
+return nil
+}
